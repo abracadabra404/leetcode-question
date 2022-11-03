394399e2f99bf7e1339a00f48fe8f9fd26a0af4c
github <xvyaxi@outlook.com>
2022-11-03 14:49:06 +0800

    update .gitignore

---
+++ src/main/java/com/xyx/leetcode/editor/cn/Q21MergeTwoSortedLists.java
@@ -0,0 +1,97 @@
+  //将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [], l2 = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两个链表的节点数目范围是 [0, 50] 
+// -100 <= Node.val <= 100 
+// l1 和 l2 均按 非递减顺序 排列 
+// 
+//
+// Related Topics递归 | 链表 
+//
+// 👍 2768, 👎 0bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
+
+  
+  package com.xyx.leetcode.editor.cn;
+
+  import com.xyx.leetcode.ListNode;
+
+  public class Q21MergeTwoSortedLists{
+      public static void main(String[] args) {
+           Solution solution = new Q21MergeTwoSortedLists().new Solution();
+          ListNode l1 = new ListNode(2, new ListNode(4));
+          ListNode list1 = new ListNode(1, l1);
+          ListNode l2 = new ListNode(3, new ListNode(4));
+          ListNode list2 = new ListNode(1, l2);
+          System.out.println(solution.mergeTwoLists(list1,list2));
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1), p = dummy;
+        ListNode p1 = list1, p2 = list2;
+        while (p1 != null && p2 != null) {
+            // 比较p1,p2两个指针，将值较小的节点连接到p指针
+            if (p1.val > p2.val){
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            // p指针不断前进
+            p = p.next;
+        }
+        if (p1 != null) {
+            p.next = p1;
+        }
+        if (p2 != null) {
+            p.next = p2;
+        }
+        return dummy.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
