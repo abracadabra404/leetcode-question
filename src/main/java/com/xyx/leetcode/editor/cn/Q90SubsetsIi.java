36175acf2e67fb3d3b021bd4daaf63135799d354
xvyaxi <xvyaxi@outlook.com>
2022-11-16 12:58:49 +0800

    update doc

--- src/main/java/com/xyx/leetcode/editor/cn/Q90SubsetsIi.java
+++ src/main/java/com/xyx/leetcode/editor/cn/Q90SubsetsIi.java
@@ -1,4 +1,4 @@
-  //给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。 
+//给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
 //
 // 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。 
 //
@@ -39,47 +39,51 @@
 //
 //
 
-  
-  package com.xyx.leetcode.editor.cn;
 
-  import java.util.Arrays;
-  import java.util.LinkedList;
-  import java.util.List;
+package com.xyx.leetcode.editor.cn;
 
-  public class Q90SubsetsIi{
-      public static void main(String[] args) {
-           Solution solution = new Q90SubsetsIi().new Solution();
-          int[] nums = {1, 2, 2};
-          System.out.println(solution.subsetsWithDup(nums));
-      }
-      //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
-          List<List<Integer>> res = new LinkedList<>();
-          // 记录路径
-          LinkedList<Integer> track = new LinkedList<>();
-
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        backtrack(nums, 0);
-        return res;
+public class Q90SubsetsIi {
+    public static void main(String[] args) {
+        Solution solution = new Q90SubsetsIi().new Solution();
+        int[] nums = {1, 2, 2};
+        System.out.println(solution.subsetsWithDup(nums));
     }
 
-          private void backtrack(int[] nums, int start) {
-                  res.add(new LinkedList<>(track));
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
 
-              for (int j = start; j < nums.length; j++) {
-                  if (j > start && nums[j] == nums[j - 1]) {
-                      continue;
-                  }
-                  track.addLast(nums[j]);
-                  backtrack(nums,j + 1);
-                  track.removeLast();
-              }
-          }
+        List<List<Integer>> res = new LinkedList<>();
+        // 记录路径
+        LinkedList<Integer> track = new LinkedList<>();
+
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            // 排序，相同 的元素排在一起
+            Arrays.sort(nums);
+            backtrack(nums, 0);
+            return res;
+        }
+
+        private void backtrack(int[] nums, int start) {
+            // 前序位置，每个节点的值都是一个子集
+            res.add(new LinkedList<>(track));
+
+            for (int j = start; j < nums.length; j++) {
+                // 剪枝逻辑，值相同的相邻树枝，只遍历第一条
+                if (j > start && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                track.addLast(nums[j]);
+                backtrack(nums, j + 1);
+                track.removeLast();
+            }
+        }
 
 
-      }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-  }
+}
