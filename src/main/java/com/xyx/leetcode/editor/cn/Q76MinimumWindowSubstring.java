fd1f155e2bb9a72300dfc66e5f24c0311e626d1f
xvyaxi <xvyaxi@outlook.com>
2022-11-21 21:31:49 +0800

    update minimum window substring doc

--- src/main/java/com/xyx/leetcode/editor/cn/Q76MinimumWindowSubstring.java
+++ src/main/java/com/xyx/leetcode/editor/cn/Q76MinimumWindowSubstring.java
@@ -77,26 +77,32 @@ class Solution {
         }
         int left = 0, right = 0;
         int valid = 0;
-
+        // 记录最小覆盖字串的起始索引以及长度
         int start = 0, len = Integer.MAX_VALUE;
         while (right < sources.length) {
-
+            // c是将移入窗口的字符
             char c = sources[right];
+            // 扩大窗口
             right++;
+            // 进行窗口内的数据一系列更新
             if (need.containsKey(c)) {
                 window.put(c, window.getOrDefault(c, 0) + 1);
                 if (window.get(c).equals(need.get(c))) {
                     valid++;
                 }
             }
-
+            // 判断左侧窗口是否要收缩
             while (valid == need.size()) {
+                // 这里更新最小覆盖字串
                 if (right - left < len) {
                     start = left;
                     len = right - left;
                 }
+                // d是将移出窗口的字符
                 char d = sources[left];
+                // 缩小窗口
                 left++;
+                // 进行窗口内的一系列更新
                 if (need.containsKey(d)) {
                     if (window.get(d).equals(need.get(d))) {
                         valid --;
@@ -104,8 +110,8 @@ class Solution {
                     window.put(d, window.getOrDefault(d,0) - 1);
                 }
             }
-
         }
+        // 返回最小覆盖字串
         return len == Integer.MAX_VALUE ? "" : s.substring(start, start+len);
     }
 }
