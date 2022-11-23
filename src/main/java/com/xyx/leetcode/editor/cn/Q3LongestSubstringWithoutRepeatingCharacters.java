8502eaa9738c3a6200616ceeb4269158d66566ca
xvyaxi <xvyaxi@outlook.com>
2022-11-23 20:07:06 +0800

    Q3 doc

--- src/main/java/com/xyx/leetcode/editor/cn/Q3LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/xyx/leetcode/editor/cn/Q3LongestSubstringWithoutRepeatingCharacters.java
@@ -60,19 +60,22 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         char[] chars = s.toCharArray();
         HashMap<Character, Integer> window = new HashMap<>();
-
         int left = 0, right = 0;
         // 记录结果
         int res = 0;
         while (right < chars.length) {
             char c = chars[right];
             right++;
+            // 进行窗口内数据的一系列更新
             window.put(c, window.getOrDefault(c, 0) + 1);
+            // 判断左侧窗口是否要收缩
             while (window.get(c) > 1) {
                 char d = chars[left];
                 left++;
+                // 进行窗口内数据的一些列更新
                 window.put(d, window.get(d) - 1);
             }
+            // 这里更新答案
             res = Math.max(res, right - left);
         }
         return res;
