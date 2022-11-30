536f45a40a311114e0e76fec2ff1f44afde91e41
xvyaxi <xvyaxi@outlook.com>
2022-11-30 21:37:29 +0800

    sell stockII optimize

--- src/main/java/com/xyx/leetcode/editor/cn/Q714BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/main/java/com/xyx/leetcode/editor/cn/Q714BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -56,23 +56,34 @@ public class Q714BestTimeToBuyAndSellStockWithTransactionFee {
         System.out.println(solution.maxProfit(prices, fee));
     }
 
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i - 1 == -1) {
+                // base case
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i] - fee;
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i] - fee);
+
+        }
+        return dp[n - 1][0];
+    }
+
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maxProfit(int[] prices, int fee) {
             int n = prices.length;
-            int[][] dp = new int[n][2];
+            int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
             for (int i = 0; i < n; i++) {
-                if (i - 1 == -1) {
-                    // base case
-                    dp[i][0] = 0;
-                    dp[i][1] = -prices[i] - fee;
-                    continue;
-                }
-                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i] - fee);
-
+                int temp = dp_i_0;
+                dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+                dp_i_1 = Math.max(dp_i_1, temp - prices[i] - fee);
             }
-            return dp[n - 1][0];
+            return dp_i_0;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
