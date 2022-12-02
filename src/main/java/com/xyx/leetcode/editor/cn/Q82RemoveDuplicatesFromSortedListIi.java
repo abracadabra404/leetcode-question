982dae92f683e0f734cb6c6b7b75aadf51584d2b
xvyaxi <xvyaxi@outlook.com>
2022-12-02 15:18:10 +0800

    删除有序列表的重复元素

---
+++ src/main/java/com/xyx/leetcode/editor/cn/Q82RemoveDuplicatesFromSortedListIi.java
@@ -0,0 +1,87 @@
+//给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,3,4,4,5]
+//输出：[1,2,5]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [1,1,1,2,3]
+//输出：[2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围 [0, 300] 内 
+// -100 <= Node.val <= 100 
+// 题目数据保证链表已经按升序 排列 
+// 
+//
+// Related Topics链表 | 双指针 
+//
+// 👍 1030, 👎 0bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
+
+
+package com.xyx.leetcode.editor.cn;
+
+import com.xyx.leetcode.ListNode;
+
+public class Q82RemoveDuplicatesFromSortedListIi {
+    public static void main(String[] args) {
+        Solution solution = new Q82RemoveDuplicatesFromSortedListIi().new Solution();
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5)))))));
+        System.out.println("before node:" + listNode);
+        System.out.println("after node:" + solution.deleteDuplicates(listNode));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            // 设置虚拟头结点
+            ListNode dummy = new ListNode(-1);
+            // 将虚拟头结点与原链接头结点连接起来
+            dummy.next = head;
+            // 从虚拟头结点开始访问
+            ListNode cur = dummy;
+            // 只要当前节点的下一个节点与下下节点都存在，就继续访问下去
+            while (cur.next != null && cur.next.next != null) {
+                // 访问过程，下一个节点与下下节点相同，说明与这个节点的值所有的节点都应该删除掉
+                if (cur.next.val == cur.next.next.val) {
+                    // 删除的方法是先记录这个值
+                    int value = cur.next.val;
+                    while (cur.next != null && cur.next.val == value) {
+                        cur.next = cur.next.next;
+                    }
+                } else {
+                    cur = cur.next;
+                }
+            }
+            return dummy.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/com/xyx/leetcode/editor/cn/doc/content/Q82RemoveDuplicatesFromSortedListIi.md
@@ -0,0 +1,43 @@
+<p>给定一个已排序的链表的头&nbsp;<code>head</code> ，&nbsp;<em>删除原始链表中所有重复数字的节点，只留下不同的数字</em>&nbsp;。返回 <em>已排序的链表</em>&nbsp;。</p>
+
+<p>&nbsp;</p>
+
+<p><strong>示例 1：</strong></p> 
+<img alt="" src="https://assets.leetcode.com/uploads/2021/01/04/linkedlist1.jpg" style="height: 142px; width: 500px;" /> 
+<pre>
+<strong>输入：</strong>head = [1,2,3,3,4,4,5]
+<strong>输出：</strong>[1,2,5]
+</pre>
+
+<p><strong>示例 2：</strong></p> 
+<img alt="" src="https://assets.leetcode.com/uploads/2021/01/04/linkedlist2.jpg" style="height: 164px; width: 400px;" /> 
+<pre>
+<strong>输入：</strong>head = [1,1,1,2,3]
+<strong>输出：</strong>[2,3]
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示：</strong></p>
+
+<ul> 
+ <li>链表中节点数目在范围 <code>[0, 300]</code> 内</li> 
+ <li><code>-100 &lt;= Node.val &lt;= 100</code></li> 
+ <li>题目数据保证链表已经按升序 <strong>排列</strong></li> 
+</ul>
+
+<details><summary><strong>Related Topics</strong></summary>链表 | 双指针</details><br>
+
+<div>👍 1030, 👎 0<span style='float: right;'><span style='color: gray;'><a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a> | <a href='https://mp.weixin.qq.com/s/NF8mmVyXVfC1ehdMOsO7Cw' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a> | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a></span></span></div>
+
+<div id="labuladong"><hr>
+
+**通知：[数据结构精品课](https://aep.h5.xeknow.com/s/1XJHEO) 已更新到 V2.0，第 14 期打卡训练营 [开始报名](https://aep.xet.tech/s/3ts7sQ)。**
+
+<details><summary><strong>labuladong 思路</strong></summary>
+
+本题思路为《[数据结构精品课](https://aep.h5.xeknow.com/s/1XJHEO)》学员专属，购买**并登录课程**后即可查看。如果你已经购买课程还无法查看，请点击 [这里](https://appktavsiei5995.pc.xiaoe-tech.com/detail/i_62b43720e4b07bd2d7b1b6dd/1) 查看各个插件的解锁方法。</details>
+</div>
+
+
+
