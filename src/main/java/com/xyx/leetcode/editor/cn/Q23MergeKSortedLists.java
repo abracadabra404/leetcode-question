fb5d4c20cec5c0097093160352d1bc96e1148da2
xvyaxi <xvyaxi@outlook.com>
2022-12-04 16:08:45 +0800

    merge K lists

--- src/main/java/com/xyx/leetcode/editor/cn/Q23MergeKSortedLists.java
+++ src/main/java/com/xyx/leetcode/editor/cn/Q23MergeKSortedLists.java
@@ -84,20 +84,25 @@ public class Q23MergeKSortedLists {
             if (lists.length == 0) {
                 return null;
             }
+            // 虚拟头结点
             ListNode dummy = new ListNode(-1);
             ListNode p = dummy;
+            // 优先级队列，最小堆
             PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, (a, b) -> (a.val - b.val));
+            // 将k个链表的头结点加入到最小堆
             for (ListNode head : lists) {
                 if (head != null) {
                     pq.add(head);
                 }
             }
             while (!pq.isEmpty()) {
+                // 获取最小结点，接到结果链表中
                 ListNode node = pq.poll();
                 p.next = node;
                 if (node.next != null) {
                     pq.add(node.next);
                 }
+                // P指针不断前进
                 p = p.next;
             }
             return dummy.next;
