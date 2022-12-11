5e18a0ab597ecd923bd78fe3a0417c990db8dfc6
xvyaxi <xvyaxi@outlook.com>
2022-12-11 14:47:51 +0800

    longest palindromic substring

---
+++ src/main/java/com/xyx/leetcode/editor/cn/Q5LongestPalindromicSubstring.java
@@ -0,0 +1,70 @@
+//给你一个字符串 s，找到 s 中最长的回文子串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "cbbd"
+//输出："bb"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 仅由数字和英文字母组成 
+// 
+//
+// Related Topics字符串 | 动态规划 
+//
+// 👍 5965, 👎 0bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
+
+
+package com.xyx.leetcode.editor.cn;
+
+public class Q5LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        Solution solution = new Q5LongestPalindromicSubstring().new Solution();
+        String s = "babad";
+        System.out.println(solution.longestPalindrome(s));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String longestPalindrome(String s) {
+            String res = "";
+            for (int i = 0; i < s.length(); i++) {
+                String s1 = palindrome(s, i, i);
+                String s2 = palindrome(s, i, i + 1);
+                res = res.length() > s1.length() ? res : s1;
+                res = res.length() > s2.length() ? res : s2;
+            }
+            return res;
+        }
+
+        public String palindrome(String s, int l, int r) {
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                l--;
+                r++;
+            }
+            return s.substring(l + 1, r);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/main/java/com/xyx/leetcode/editor/cn/doc/content/Q5LongestPalindromicSubstring.md
+++ src/main/java/com/xyx/leetcode/editor/cn/doc/content/Q5LongestPalindromicSubstring.md
@@ -32,7 +32,7 @@
 
 <div id="labuladong"><hr>
 
-**通知：[数据结构精品课](https://aep.h5.xeknow.com/s/1XJHEO) 已更新到 V2.0；[第 13 期刷题打卡](https://mp.weixin.qq.com/s/eUG2OOzY3k_ZTz-CFvtv5Q) 最后几天报名！**
+**通知：[数据结构精品课](https://aep.h5.xeknow.com/s/1XJHEO) 已更新到 V2.0，2022 年最后一期打卡训练营 [最后一天报名](https://mp.weixin.qq.com/s/eUG2OOzY3k_ZTz-CFvtv5Q)。**
 
 
 
@@ -113,6 +113,8 @@ class Solution {
 
 </details>
 </div>
+
+
 
 
 
